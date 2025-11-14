0642855bc89589684a1535e2389c9b74779cf63f
dev-eduardo <eduardobarrios1314@gmail.com>
2025-11-14 14:58:28 -0600

    feat: mejorar el componente Header con menú móvil y selección de elementos

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,72 @@
+import logo from "@/assets/logos/logo.png";
+
 export function Footer() {
     return (
-        <div>
-            <h1>Footer aqui</h1>
-        </div>
-    )
+        <footer>
+            <div className="p-6 sm:px-25 sm:py-22 gap-[30px] flex flex-col max-w-auto lg:flex-row lg:justify-between lg:items-center ">
+                <div className=" sm:flex sm:justify-between sm:items-center">
+                    <div className="flex-col  ">
+                        <img src={logo} alt="Logo" />
+
+                        <p className="mt-7 sm:mt-9 max-w-[400px] text-[#AFADB5] sm:text-title-950 font-['Eudoxus_Sans'] font-medium text-[14px] sm:text-[18px] leading-[180%] ">
+                            Lalasia is digital agency that help you make better experience
+                            iaculis cras in.
+                        </p>
+                    </div>
+                </div>
+                <div className="grid grid-cols-3  sm:gap-20 md:justify-start ">
+                    <ul className="inline-flex flex-col items-start gap-2 sm:gap-3">
+                        <li className="text-title-950 font-['Eudoxus_Sans'] sm:mb-4 text-[16px] sm:text-[20px] font-bold leading-[130%]">
+                            Product
+                        </li>
+                        <li className="text-[#AFADB5] sm:text-title-950 font-['Eudoxus_Sans'] text-[14px] sm:text-[18px] font-medium leading-[180%]">
+                            New Arrivals
+                        </li>
+                        <li className="text-[#AFADB5] sm:text-title-950 font-['Eudoxus_Sans'] text[14px] sm:text-[18px] font-medium leading-[180%]">
+                            Best Selling
+                        </li>
+                        <li className="text-[#AFADB5] sm:text-title-950 font-['Eudoxus_Sans'] text-[14px] sm:text-[18px] font-medium leading-[180%]">
+                            Home Decor
+                        </li>
+                        <li className="text-[#AFADB5] sm:text-title-950 font-['Eudoxus_Sans'] text-[14px] sm:text-[18px] font-medium leading-[180%]">
+                            Kitchen Set
+                        </li>
+                    </ul>
+
+                    <ul className="inline-flex flex-col items-start gap-2 sm:gap-3 ">
+                        <li className="text-title-950 font-['Eudoxus_Sans'] sm:mb-4 text-[16px] sm:text-[20px] font-bold leading-[130%]">
+                            Services
+                        </li>
+                        <li className="text-[#AFADB5] sm:text-title-950 font-['Eudoxus_Sans'] text-[14px] sm:text-[18px] font-medium leading-[180%]">
+                            Catalog
+                        </li>
+                        <li className="text-[#AFADB5] sm:text-title-950 font-['Eudoxus_Sans'] text-[14px] sm:text-[18px] font-medium leading-[180%]">
+                            Blog
+                        </li>
+                        <li className="text-[#AFADB5] sm:text-title-950 font-['Eudoxus_Sans'] text-[14px] sm:text-[18px] font-medium leading-[180%]">
+                            FaQ
+                        </li>
+                        <li className="text-[#AFADB5] sm:text-title-950 font-['Eudoxus_Sans'] text-[14px] sm:text-[18px] font-medium leading-[180%]">
+                            Pricing
+                        </li>
+                    </ul>
+
+                    <ul className="inline-flex flex-col items-start gap-2 sm:gap-3">
+                        <li className="text-title-950 font-['Eudoxus_Sans'] sm:mb-4 text-[16px] sm:text-[20px] font-bold leading-[130%]">
+                            Follow Us
+                        </li>
+                        <li className="text-[#AFADB5] sm:text-title-950 font-['Eudoxus_Sans'] text[14px] sm:text-[18px] font-medium leading-[180%]">
+                            Facebook
+                        </li>
+                        <li className="text-[#AFADB5] sm:text-title-950 font-['Eudoxus_Sans'] text-[14px] sm:text-[18px] font-medium leading-[180%]">
+                            Instagram
+                        </li>
+                        <li className="text-[#AFADB5] sm:text-title-950 font-['Eudoxus_Sans'] text-[14px] sm:text-[18px] font-medium leading-[180%]">
+                            Twitter
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
 }
